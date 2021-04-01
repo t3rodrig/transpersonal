eeec6f712554e3e42e010e41e3f959720024cc75
Tonalli Rodriguez <t3rodrig@users.noreply.github.com>
2021-04-01 09:28:31 -0600

    Add more pages

---
+++ src/pages/nosotros.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NosotrosPage = () => (
+  <Layout>
+    <SEO title="Nosotros" />
+    <h1>Hi from Nosotros page</h1>
+  </Layout>
+)
+
+export default NosotrosPage
---
+++ src/pages/servicios.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ServiciosPage = () => (
+  <Layout>
+    <SEO title="Servicios" />
+    <h1>Hi from Servicios page</h1>
+  </Layout>
+)
+
+export default ServiciosPage
